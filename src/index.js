717ce0184633b8dce3198484727c4c001f7a6019
olgareuts <olga.re@mail.ru>
2023-01-07 16:43:51 +0300

    refactor: formatting code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ module.exports = function toReadable (number) {
     if (number < 20) {
       return  result = result + lessTwentyNumbers [number];
     } else if (number < 100) {
-      return result = result + (number % 10 === 0 ? tensNumbers[Math.floor(number / 10-2)] : tensNumbers[Math.floor(number / 10-2)] + ' ' + lessTwentyNumbers[number % 10]);
+      return result = result + (number % 10 === 0 ? tensNumbers[Math.floor(number / 10-2)] : 
+             tensNumbers[Math.floor(number / 10-2)] + ' ' + lessTwentyNumbers[number % 10]);
     } else if (number < 1000) {
       if (number % 100 === 0) { 
         return result = result + lessTwentyNumbers[Math.floor(number / 100)] + ' ' + hundredsNumbers[0]
